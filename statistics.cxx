5f7f326b5dd6ed2ffe5092ef6aa12dfdf5c7bc47
dervonderTankstelle <Viktor.Pfaffenrot@hhu.de>
2015-11-15 16:46:13 +0100

    Servus. Hoffe das passt so

--- statistics.cxx
+++ statistics.cxx
@@ -3,16 +3,55 @@
 #include <cstdlib>
 
 using namespace std;
-//--------------------------------------------------
-// plenty of lines here
-//--------------------------------------------------
+
+void Random(const int N, double* p) // Kreiert beliebige ganze Zahlen in 
+                                    // einem Array                            
+{
+    for(int i = 0; i < N; i++)
+    {
+        p[i] = rand();
+    }
+    
+
+}
+
+double Mittel(const int N, double* p)
+{
+    double sum = 0;
+    for(int i = 0; i < N; i++)
+    {   
+        sum = sum + p[i];
+    }
+    
+    return sum/N;
+}
+
+double Varianz(const int N, double* p)
+{
+    double sum = 0;
+    double mean = Mittel(N,p);
+    for(int i = 0; i < N; i++)
+    {
+        sum = sum + pow((p[i] - mean),2);
+    }
+
+    return sum/N;
+}
+
 
 int main(){
+    
    const int N = 100;
    double p[N];
    double mean, var;
-
-   // Some lines here....
+   
+   
+   srand(time(NULL));
+   
+   Random(N,p);
+   
+   mean = Mittel(N,p);
+   var = Varianz(N,p);
 
    cout << "Mean = " << mean << endl;
    cout << "Variance = " << var << endl;
